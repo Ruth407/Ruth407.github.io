c12734ed353e80dd79f2334b07bac8fbcb4f0766
Ruth Ramirez <ruthralcazar@gmail.com>
2026-02-22 23:39:06 -0800

    add lab3 quiz

---
+++ lab3-quiz/js/script.js
@@ -0,0 +1,116 @@
+//event listeners
+document.querySelector("button").addEventListener("click", gradeQuiz);
+
+//Global variables
+var score =0;
+var attempts = localStorage.getItem("total_attempts");
+
+
+displayQ4Choices();
+
+//functions
+function displayQ4Choices() {
+    let q4ChoicesArray = ["Asia", "Africa", "Europe", "North America"];
+    q4ChoicesArray = _.shuffle(q4ChoicesArray);
+
+    for (let i = 0; i < q4ChoicesArray.length; i++) {
+        document.querySelector("#q4Choices").innerHTML += 
+        `<input type="radio" name="q4" id="${q4ChoicesArray[i]}" value="${q4ChoicesArray[i]}">
+         <label for="${q4ChoicesArray[i]}">${q4ChoicesArray[i]}</label><br>`;
+    }
+}
+
+
+//Functions
+function isFormValid(){
+    let isValid = true;
+    if(document.querySelector("#q1").value == ""){
+        isValid = false;
+    document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered";
+    }
+    return isValid;
+}
+
+function rightAnswer(index){
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
+
+    score += 20;
+}
+
+function wrongAnswer(index){
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt='Xmark'>";
+
+}
+
+
+function gradeQuiz() {
+    console.log("Grading quiz...")
+    document.querySelector("#validationFdbk").innerHTML =""; //resets validation feedback
+    
+    if(!isFormValid()) {
+        return;
+    }
+
+
+    //variables
+    score = 0;
+    let q1Response = document.querySelector("#q1").value.toLowerCase();
+    let q2Response = document.querySelector("#q2").value.toLowerCase();
+    console.log(q2Response);
+    let q4Response = document.querySelector("input[name=q4]:checked").value;
+
+    //Grading question 1
+    if(q1Response == "jupiter") {
+        rightAnswer(1);
+    } else {
+        wrongAnswer(1);
+    }
+
+    //Grading question 2
+    if(q2Response == "dolphin") {
+        rightAnswer(2);
+    } else {
+        wrongAnswer(2);
+    }
+
+    //Grading question 3
+    if(document.querySelector("#Red").checked &&
+       document.querySelector("#Blue").checked &&
+       !document.querySelector("#Green").checked &&
+       !document.querySelector("#Purple").checked) {
+
+        rightAnswer(3);
+    } else {
+        wrongAnswer(3);
+    }
+
+    //Grading question 4
+    if(q4Response == "Asia") {
+        rightAnswer(4);
+    } else {
+        wrongAnswer(4);
+    }
+
+    document.querySelector("#totalScore").innerHTML =`Total Score: ${score}`;    
+    document.querySelector("#totalAttempts").innerHTML =`Total Attempts: ${++attempts}`;   
+    localStorage.setItem("total_attempts", attempts); 
+
+if (score < 80) {
+    document.querySelector("#totalScore").className = "text-danger";  // red
+} else {
+    document.querySelector("#totalScore").className = "text-success"; // green
+}
+
+if (score > 80) {
+    document.querySelector("#totalScore").innerHTML += "<br>Good job!";
+}
+
+}
+
+
+
+
